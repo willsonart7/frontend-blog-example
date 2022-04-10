f3e105051653c5520c06ca4a1af271998a2d3252
Willson Arteaga <willsonart7@gmail.com>
2022-04-10 00:22:52 -0400

    Inicial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import PostList from './components/posts/list'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>Init APP</div>
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <div>
+          <h2 className='text-2xl font-semibold text-blue-700'>
+            Example admin posts
+          </h2>
+        </div>
+        <div className="flex flex-col">
+          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="overflow-hidden">
+                <div>search</div>
+                <PostList />
+                <div>input</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
